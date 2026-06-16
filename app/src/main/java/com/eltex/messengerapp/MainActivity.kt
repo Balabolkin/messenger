f445944f292fb2b96000ccc2f2b88303744d5191
ivanistrator <peregoedov20@gmail.com>
2026-06-16 12:41:34 +0700

    Initial commit architecture

---
+++ app/src/main/java/com/eltex/messengerapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.eltex.messengerapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.eltex.messengerapp.ui.theme.BrandPrimary
+import com.eltex.messengerapp.ui.theme.MessengerAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MessengerAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Text(modifier = modifier.background(color = BrandPrimary), text = "Hello")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MessengerAppTheme {
+        Greeting()
+    }
+}
