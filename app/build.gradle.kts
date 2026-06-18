f59d53227524f7659fde2b042229d60d8da14844
Sofya Savchenko <sofi.savchenko@yandex.ru>
2026-06-18 07:15:51 +0700

    versions kttorBom okhttpBom hiltLifecycleViewmodelCompose material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,15 +18,27 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
 
+        buildConfigField("String", "API_KEY", "\"YOUR_API_KEY\"")
+        buildConfigField("String", "Authorization", "\"Bearer YOUR_TOKEN\"")
+    }
+    buildFeatures {
+        buildConfig = true
+    }
     buildTypes {
+        debug {
+            buildConfigField("boolean", "DEBUG", "true")
+        }
         release {
+<<<<<<< HEAD
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+=======
+            buildConfigField("boolean", "DEBUG", "false")
+>>>>>>> d56c392 (versions kttorBom okhttpBom hiltLifecycleViewmodelCompose material3)
         }
     }
     compileOptions {
@@ -34,16 +46,18 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
+    implementation(libs.androidx.material3)
     coreLibraryDesugaring(libs.desugar.jdk.libs)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
+<<<<<<< HEAD
+=======
+    runtimeOnly(libs.androidx.compose.material.icons.extended)
+>>>>>>> d56c392 (versions kttorBom okhttpBom hiltLifecycleViewmodelCompose material3)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
@@ -55,11 +69,24 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.hilt.android)
+<<<<<<< HEAD
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.datastore.preferences)
+=======
+    implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
+>>>>>>> d56c392 (versions kttorBom okhttpBom hiltLifecycleViewmodelCompose material3)
     ksp(libs.hilt.compiler)
+
+    implementation(platform(libs.ktor.bom))
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.json)
+    implementation(libs.ktor.client.logging)
+
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.logging.interceptor)
 
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
