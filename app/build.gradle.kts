5d6d214b8e2b57c0a7e1781b96d154105259da40
Sofya Savchenko <sofi.savchenko@yandex.ru>
2026-06-24 19:33:18 +0700

    Resolving conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,15 +44,12 @@ android {
             buildConfigField("boolean", "DEBUG", "true")
         }
         release {
-<<<<<<< HEAD
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-=======
             buildConfigField("boolean", "DEBUG", "false")
->>>>>>> d56c392 (versions kttorBom okhttpBom hiltLifecycleViewmodelCompose material3)
         }
     }
 
@@ -70,10 +67,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
-<<<<<<< HEAD
-=======
     runtimeOnly(libs.androidx.compose.material.icons.extended)
->>>>>>> d56c392 (versions kttorBom okhttpBom hiltLifecycleViewmodelCompose material3)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
@@ -85,14 +79,11 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.hilt.android)
-<<<<<<< HEAD
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.datastore.preferences)
-=======
     implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
->>>>>>> d56c392 (versions kttorBom okhttpBom hiltLifecycleViewmodelCompose material3)
     ksp(libs.hilt.compiler)
 
     implementation(platform(libs.ktor.bom))
