69d6e3a2a6b2f172d45c07eda537c3b7e3b48a5a
ivanistrator <peregoedov20@gmail.com>
2026-07-02 20:46:08 +0700

    Work with users

---
+++ app/src/main/java/com/eltex/messengerapp/feature/user/data/UsersApi.kt
@@ -0,0 +1,47 @@
+package com.eltex.messengerapp.feature.user.data
+
+import android.util.Log
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.url
+import kotlinx.serialization.Serializable
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Serializable
+data class UsersAutocompleteResponse(
+    val items: List<UserDto>,
+    val success: Boolean
+)
+
+@Serializable
+data class UsersListResponse(
+    val users: List<UserDto>,
+    val count: Int,
+    val offset: Int,
+    val total: Int,
+    val success: Boolean
+)
+
+@Singleton
+class UsersApi @Inject constructor(
+    private val client: HttpClient
+) {
+    suspend fun searchUsers(query: String): UsersAutocompleteResponse {
+        return client.get {
+            url("api/v1/users.autocomplete")
+            parameter("selector", """{"term":"$query"}""")
+        }.body()
+    }
+
+    suspend fun getUsers(): UsersListResponse {
+        val response = client.get {
+            url("api/v1/users.list")
+            parameter("count", 20)
+        }
+        val body = response.body<String>()
+        return response.body()
+    }
+}
